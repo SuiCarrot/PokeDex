0e65017207c7f19823d89a5ab574a68137b32976
SuiCarrot <91708805+SuiCarrot@users.noreply.github.com>
2022-03-17 22:55:32 -0300

    Att detalhes/css/index

--- index.js
+++ index.js
@@ -12,33 +12,39 @@ const pokedex = [
         nome: "Bulbasaur",
         tipo: "Grass and Poison",
         imagem: "img/bulbasaur.png",
+        imagemAlt: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
         descricao: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
         altura: "0.7m",
         peso: "6.9kg",
         categoria: "Seed",
-        habilidade: "Overgrow" 
+        habilidade: "Overgrow",
+        id: 0
     },
     {   
         numero: "004",
         nome: "Charmander",
         tipo: "Fire",
         imagem: "img/charmander.png",
+        imagemAlt: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
         descricao: "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
         altura: "0.6m",
         peso: "8.5kg",
         categoria: "Lizard",
-        habilidade: "Blaze"
+        habilidade: "Blaze",
+        id: 1,        
     },
     {   
       numero: "007",
       nome: "Squirtle",
       tipo: "Water",
       imagem: "img/squirtle.png",
+      imagemAlt: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
       descricao: "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
       altura: "0.5m",
       peso: "9.0kg",
       categoria: "Tiny Turtle",
-      habilidade: "Torrent"
+      habilidade: "Torrent",
+      id: 2
   },
 ]
 
