9cd67f3d78ca125e42c4c0790282d25fc0cf9b88
Carlos-xbm <carloseduardoedu.carlos@hotmail.com>
2022-03-25 20:23:25 -0300

    v1

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,11 @@
 const closeMessage = document.querySelector(".close");
 const message = document.querySelector(".mensagem");
 
-closeMessage.addEventListener("click", function (){
-    message.style.display = "none";
+closeMessage.addEventListener("click", function () {
+  message.style.display = "none";
 });
 
 setTimeout(() => {
-    message.style.display = "none";
-    location.reload();
+  message.style.display = "none";
+  location.reload();
 }, 3000);
