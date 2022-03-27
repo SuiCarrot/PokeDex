61506edf6566a6fef5cda8262656b1ca6ba06b7a
SuiCarrot <91708805+SuiCarrot@users.noreply.github.com>
2022-03-27 11:12:16 -0300

    att js

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,11 @@
 const closeMessage = document.querySelector(".close");
 const message = document.querySelector(".mensagem");
 
-<<<<<<< HEAD
-closeMessage.addEventListener("click", function () {
-  message.style.display = "none";
-});
-
-setTimeout(() => {
-  message.style.display = "none";
-  location.reload();
-}, 3000);
-=======
 closeMessage.addEventListener("click", () => {
     message.style.display = "none";
 });
 
 setTimeout(() => {
-    message.style.display = "none !important";
+    message.style.display = "none";
     location.reload();
 }, 5000);
->>>>>>> 672fb037465b44b2d2efe3aa15a775b89e9c3adf
